4dec69ee5cae2de6e4c4d24b3cb9e3e099e5635a
Dylan Stoker <dmstoker88@gmail.com>
2019-11-06 05:32:04 +0000

    Nomster 29

--- Gemfile
+++ Gemfile
@@ -2,16 +2,18 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.3'
-
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.2.3'
-gem 'jquery-rails'
 gem 'will_paginate'
+gem 'carrierwave', '~> 2.0'
+gem 'fog-aws'
+gem 'mini_magick'
 gem 'simple_form'
-gem "font-awesome-rails"
+gem 'jquery-rails'
+gem 'font-awesome-rails'
 gem 'devise'
 gem "geocoder"
-
+gem "figaro"
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '~> 5.2.3'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+     before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
   @places = Place.order("name").page(params[:page]).per_page(8)
@@ -10,9 +10,55 @@ end
     @place = Place.new
   end
 
- def create
-     current_user.places.create(place_params)
+ 
+
+  def create
+  @place = current_user.places.create(place_params)
+  if @place.valid?
     redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+end
+
+  def show
+     @place = Place.find(params[:id])
+    # @comment = Comment.new
+    #@photo = Photo.new
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+    if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
+  end
+
+  def update
+  @place = Place.find(params[:id])
+  if @place.user != current_user
+  return render plain: 'Not Allowed', status: :forbidden
+  else
+  @place.update_attributes(place_params)
+  redirect_to root_path
+  end
+end
+
+  #@place.update_attributes(place_params)
+  #f @place.valid?
+    #redirect_to root_path
+  #else
+   # render :edit, status: :unprocessable_entity
+  #end
+#end
+
+  def destroy
+  @place = Place.find(params[:id])
+  if @place.user != current_user
+  return render plain: 'Not Allowed', status: :forbidden
+  end
+  @place.destroy
+  redirect_to root_path
   end
 
   private
