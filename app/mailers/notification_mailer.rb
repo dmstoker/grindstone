05882baa4fc69014617703a2ba0297b66d355641
dmstoker <dmstoker88@gmail.com>
2019-11-08 08:52:43 +0000

    nomster 44

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,13 @@
 class NotificationMailer < ApplicationMailer
-  default from: "no-reply@nomsterapp.com"
+  default from: "no-reply@nGrindingGrounds.com"
 
-def comment_added(comment)
+  def comment_added(comment)
   @place = comment.place
   @place_owner = @place.user
   mail(to: @place_owner.email,
-       subject: "A comment has been added to #{@place.name}")
+       subject: "A comment has been added to your #{@place.name}")
 end
+
 end
+
+
