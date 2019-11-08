7201ff4abd5d77d186d1c6382090ecd95815d4c9
dmstoker <dmstoker88@gmail.com>
2019-11-08 10:48:00 +0000

    nomster 44

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ ActionMailer::Base.smtp_settings = {
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
