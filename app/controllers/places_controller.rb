6670ec2f36413f9b35baa24d7ccbdd41eb2fa861
dmstoker <dmstoker88@gmail.com>
2019-11-14 08:18:24 +0000

    Nomster 47

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -36,23 +36,19 @@ end
   end
   end
 
-  def update
+ def update
   @place = Place.find(params[:id])
   if @place.user != current_user
-  return render plain: 'Not Allowed', status: :forbidden
+    return render plain: 'Not Allowed', status: :forbidden
+  end
+
+   @place.update_attributes(place_params)
+  if @place.valid?
+    redirect_to root_path
   else
-  @place.update_attributes(place_params)
-  redirect_to root_path
+    render :edit, status: :unprocessable_entity
   end
 end
-
-  #@place.update_attributes(place_params)
-  #f @place.valid?
-    #redirect_to root_path
-  #else
-   # render :edit, status: :unprocessable_entity
-  #end
-#end
 
   def destroy
   @place = Place.find(params[:id])
