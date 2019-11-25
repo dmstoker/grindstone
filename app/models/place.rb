5b7565b0c50eb22c6b4c1dee20c1816267f3c5ac
dmstoker <dmstoker88@gmail.com>
2019-11-25 03:50:49 +0000

    Nomster almost

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,9 @@
 class Place < ApplicationRecord
 belongs_to :user
-has_many :comments
-has_many :photos
-geocoded_by :address
-after_validation :geocode
+has_many :comments :dependent => :delete_all
+has_many :photos  :dependent => :delete_all
+geocoded_by :address :dependent => :delete_all
+after_validation :geocode :dependent => :delete_all
 
 validates :name, presence: true, length: {minimum: 3 }
 validates :address, presence: true
